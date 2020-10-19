b629cb4bc54ded67f72184af84437750c6d345d7
yuyaebean <yuyaebean@gmail.com>
2020-10-19 22:28:06 +0900

    2.4 Indroducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
