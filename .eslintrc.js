a4178ca4ac88f666ef593f5281a3ff1e9bc31bff
yuyaebean <yuyaebean@gmail.com>
2020-12-13 17:23:09 +0900

    3.2 Injecting The Repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -22,5 +22,16 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
   },
 };
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,15 +1,19 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+
 import { CreateRestaurnatDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
-@Resolver(() => Restaurant)
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Query(returns => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
-  @Mutation(() => Boolean)
+  @Mutation(returns => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurnatDto): boolean {
     console.log(createRestaurantDto);
     return true;
