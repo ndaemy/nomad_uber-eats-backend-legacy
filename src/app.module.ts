f219aa46c1c3f896930f5e6032e740b9bfdbb935
yuyaebean <yuyaebean@gmail.com>
2020-12-13 18:00:01 +0900

    3.3 Recap

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
+      logging: process.env.NODE_ENV !== 'prod',
       entities: [Restaurant],
     }),
     RestaurantsModule,
